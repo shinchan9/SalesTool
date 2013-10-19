9f2b4acd8d7584e70888ae63ae6cdc959bf8b7fa
Eddie <sseddiehong@gmail.com>
2013-10-19 17:11:34 -0400

    Initial checking in sales tool.

---
+++ TestProcessing/src/MyProcessingSketch.java
@@ -0,0 +1,32 @@
+import processing.core.*;
+
+
+
+public class MyProcessingSketch extends PApplet {
+
+	  public static void main(String args[]) {
+		    PApplet.main(new String[] { "--present", "MyProcessingSketch" });
+		  }	
+	
+	
+	
+	//	An array of stripes
+	Stripe[] stripes = new Stripe[50];
+
+	public void setup() {
+		size(1366,768);
+		// Initialize all "stripes"
+		for (int i = 0; i < stripes.length; i++) {
+			stripes[i] = new Stripe(this);
+		}
+	}
+
+	public void draw() {
+		background(100);
+		// Move and display all "stripes"
+		for (int i = 0; i < stripes.length; i++) {
+			stripes[i].move();
+			stripes[i].display();
+		}
+	}
+}
